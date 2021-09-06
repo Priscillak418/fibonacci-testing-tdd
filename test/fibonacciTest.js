0330591f7dfc97753279d25f0d4935e5e9824319
Priscilla Kayegi <p.kayegi@student.ciu.ac.ug>
2021-09-06 19:15:56 +0300

    adds my fibonacci test file

---
+++ test/fibonacciTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const fibonacci = require('../fibonacci');
+
+describe('fibonacci',() =>{
+    it("should return 2 as the fibonacci for 3", () => {
+        assert.equal(fibonacci.computeFibonacci(3), 2);
+    });
+    it("should return 3 as the fibonacci for 4", () => {
+        assert.equal(fibonacci.computeFibonacci(4), 3);
+    });
+    it("should return 5 as the fibonacci for 5", () => {
+        assert.equal(fibonacci.computeFibonacci(5), 5);
+    });
+});
